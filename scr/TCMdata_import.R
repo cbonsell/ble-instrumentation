bc05ebae96ff580a15939be84d224a216ffa5f1f
Christina Bonsell <cbonsell@utexas.edu>
2019-10-15 17:08:29 -0500

    added more comments

--- scr/TCMdata_import.R
+++ scr/TCMdata_import.R
@@ -5,6 +5,10 @@
 library(tidyverse)
 library(lubridate)
 
+########################
+###TCM Temperature###
+########################
+
 filelist =list.files(path = ".", recursive = TRUE,
                      pattern = "\\Temperature.csv$", 
                      full.names = TRUE)
@@ -15,6 +19,9 @@ TCM_Temp <- filelist %>%
 TCM_Temp<-TCM_Temp%>%mutate(Station=str_sub(FileName,21,25))%>%
   select(Station, DT=`ISO 8601 Time`, Temperature=`Temperature (C)`)
 
+########################
+###TCM Currents###
+########################
 filelist =list.files(path = ".", recursive = TRUE,
                      pattern = "\\Current.csv$", 
                      full.names = TRUE)
@@ -25,3 +32,9 @@ TCM_Curr <- filelist %>%
 TCM_Curr<-TCM_Curr%>%mutate(Station=str_sub(FileName,21,25))%>%
   select(Station, DT=`ISO 8601 Time`, Speed_cm_s=`Speed (cm/s)`, Heading= `Heading (degrees)`,
          Vel_N=`Velocity-N (cm/s)`, Vel_E=`Velocity-E (cm/s)`)
+
+# Settings from Lowell software data conversion:
+# TCMs: 0 ballast, Salt water
+# Declination (WMM, 1 April 2019) set for TCMS: 
+#   EEL and EWL: 13.86
+#   KAL and JAL:19.07 
